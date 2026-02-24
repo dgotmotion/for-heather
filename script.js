7310d48d6e76a7ba125b762512e5e93d61b0a5cd
dgotmotion <mukakavarid@gmail.com>
2026-02-24 12:30:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,9 +6,10 @@ document.getElementById('loveBtn').addEventListener('click', function() {
     messageDiv.innerHTML = "I love you so much! ❤️";
     
     // Optional: Add a little alert pop-up
-    alert("Heather, you are an amazing wife and i love you!");
+    alert("Heather, you are so perfect and im so lucky to able to call you my gf, me and you forever!❤️");
     
     // Change button text after click
     this.innerText = "Sent! ✨";
     this.style.backgroundColor = "#d63384";
 });
+
